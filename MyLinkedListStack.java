eb35705e07ae0c182b4227dde5d08aee7865b416
Aisha <kalimatovaaisha@mail.ru>
2023-05-01 22:50:34 +0600

    Implement the peek method

--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -20,5 +20,10 @@ public class MyLinkedListStack<T> {
         }
         return stack.removeFirst();
     }
-
+    public T peek() {
+        if (stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.getFirst();
+    }
 }
